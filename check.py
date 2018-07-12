66911181152ca4e2339b1b7cfe3dddbbebc58670
Ghanshyam <g8ghanshym@gmail.com>
2018-07-12 12:34:42 +0530

    Initial commit

---
+++ check.py
@@ -0,0 +1,39 @@
+import gzip
+import json
+
+def parse(path):
+	g = gzip.open(path, 'r')
+	for l in g:
+		yield eval(l)
+
+def no_items_reviewed(file):
+	items = []
+	no_of_items = {}
+	
+	line_no = 0
+	for line in file:
+		json_line = json.loads(json.dumps(line))
+		item = str(json_line['asin'])
+	
+		if item in items:
+			no_of_items[item] += 1
+		else:
+			items.append(item)
+			no_of_items[item] = 1
+		
+		line_no += 1
+		if line_no % 100000 == 0:
+			print(line_no)
+			print(no_of_items)
+			print(len(items))
+
+	return items, no_of_items
+
+def main():
+	file = parse("/media/dell/Seagate Expansion Drive/CE_paper/Amazon Dataset/categories/Books/reviews.json.gz")
+	items, no_of_items = no_items_reviewed(file)
+	print(no_of_items)
+	print(len(items))
+
+if __name__ == "__main__":
+	main()
