9d0957e0ecadb6d19a619b0e024006c93c151b06
Ghanshyam <g8ghanshym@gmail.com>
2018-07-18 16:25:41 +0530

    unixtime.py updated with documentations

--- unixtime.py
+++ unixtime.py
@@ -2,72 +2,86 @@ import datetime
 
 def get_date(date_time):
 	"""
+	It will return date from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		date (string): Date from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[0].split('-')[2]
 
 def get_month(date_time):
 	"""
+	It will return month from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		month (string): Month from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[0].split('-')[1]
 
 def get_year(date_time):
 	"""
+	It will return year from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		year (string): Year from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[0].split('-')[0]
 
 def get_hour(date_time):
 	"""
+	It will return hour from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		hour (string): Hour from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[1].split(':')[0]
 
 def get_minute(date_time):
 	"""
+	It will return minute from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		minute (string): Minute from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[1].split(':')[1]
 
 def get_second(date_time):
 	"""
+	It will return seconds from the date and time %Y-%m-%d %H:%M:%S format.
 	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		seconds (string): Seconds from the %Y-%m-%d %H:%M:%S format
 	"""
 	return date_time.split()[1].split(':')[2]
 
 def get_day(date_time):
 	"""
-	Monday is 1 and Sunday is 7
-
+	It will return day by using the date and time %Y-%m-%d %H:%M:%S format.
+	
 	Args:
+		date_time (string): Date and time in %Y-%m-%d %H:%M:%S format 
 
 	Returns:
-
+		month (int): Day from the %Y-%m-%d %H:%M:%S format
+					 Monday is 1 and Sunday is 7
 	"""
 	year = get_year(date_time)
 	month = get_month(date_time)
@@ -80,13 +94,6 @@ def convert(timestamp):
 	return str(date_time)
 
 def test():
-	"""
-	
-	Args:
-
-	Returns:
-
-	"""
 	timestamp = "1131750079"
 	date_time = convert(timestamp)
 	print(date_time)
