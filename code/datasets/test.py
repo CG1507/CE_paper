2eddf808e68256ba78b867abaedb676d75e582be
Ghanshyam <g8ghanshym@gmail.com>
2018-08-30 22:23:51 +0530

    :fire:

--- code/datasets/test.py
+++ code/datasets/test.py
@@ -14,15 +14,26 @@ def analyze_categories(data_folder, category_names):
 	for category in category_names:
 		subcategories[category] = []
 
+	not_price = {}
+	line_no = {}
 	for category in category_names:
-		file_pointer = parse(data_folder + category + '/reviews.json.gz')
+		file_pointer = parse(data_folder + category + '/meta.json.gz')
+		not_price[category] = 0
+		line_no[category] = 0
 		for line in file_pointer:
 			try:
 				json_line = json.loads(json.dumps(line))
-				name = json_line['reviewerName']
-				print(name)
+				price = json_line['price']
 			except:
+				not_price[category] += 1
+				line_no[category] += 1
+				print(line)
+				break
 				pass
+			line_no[category] +=1
+		print(line_no[category])
+		print(not_price[category])
+
 
 def test():
 	data_folder = "/media/dell/Seagate Expansion Drive/CE_paper/Dataset/Amazon Dataset/categories/"
