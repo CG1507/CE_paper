c6edbf160a2f5cab99dac942d4e54fecf28d56a2
Ghanshyam <g8ghanshym@gmail.com>
2018-09-01 02:34:25 +0530

    :fire: add kmeans

--- code/datasets/price_scale.py
+++ code/datasets/price_scale.py
@@ -1,5 +1,6 @@
 import datasets
 from utils import io
+from sklearn.cluster import KMeans
 import gzip
 import json
 import pickle
@@ -15,45 +16,90 @@ def parse(filepath):
 		yield eval(l)
 
 def get_price_scale(data_folder, final_category_dir, category_names):
-	all_categories = {}
-	sum = {}
+	all_info = {}
 	for category in category_names:
 		reading_meta_file_pointer = parse(data_folder + category + '/meta.json.gz')
 		print('Reading ' + category + ' metadata...')
 
+		prices = []
 		for line in reading_meta_file_pointer:
 			try:
 				json_line = json.loads(json.dumps(line))
-				related = json_line['related']
-				salesrank = json_line['salesRank']
-				brand = json_line['brand']
-				categories = json_line['categories']
 				asin = json_line['asin']
 				price = json_line['price']
-				json_dump = {}
-				json_dump[asin] = price
-				json_dump['price_level'] = 0
-				for cat in categories:
-					directory = final_category_dir + '/'.join(cat) + '/' + brand + '/'
-					create_dir(directory)
-					if directory in all_categories:
-						writing_file_pointer = io.append_file(directory + 'price.json')
-						io.write_line(writing_file_pointer, json.dumps(json_dump))
-						all_categories[directory] += 1
-						sum[directory] += price
-						writing_file_pointer.close()
-					else:
-						writing_file_pointer = io.create_file(directory + 'price.json')
-						io.write_line(writing_file_pointer, json.dumps(json_dump))
-						all_categories[directory] = 1
-						sum[directory] = price
-						writing_file_pointer.close()
+				prices.append(price)
 			except:
 				pass
 
+		if len(prices) != 0:
+			print('Clustering...')
+			temp = []
+			for item in prices:
+				temp.append([item])
+
+			kmeans = KMeans(n_clusters = 3, random_state = 0).fit(temp)
+			labels = kmeans.labels_
+			class0 = []
+			class1 = []
+			class2 = []
+			
+			for i in range(len(prices)):
+				if labels[i] == 0:
+					class0.append(prices[i])
+					continue
+
+				if labels[i] == 1:
+					class1.append(prices[i])
+					continue
+
+				if labels[i] == 2:
+					class2.append(prices[i])
+					continue
+
+			class0.sort()
+			class1.sort()
+			class2.sort()
+
+			find_cat = []
+			find_cat.append(class0[0])
+			find_cat.append(class0[-1])
+			find_cat.append(class1[0])
+			find_cat.append(class1[-1])
+			find_cat.append(class2[0])
+			find_cat.append(class2[-1])
+			find_cat.sort()
+
+			info = {}
+			if find_cat.index(class0[0]) in [0,1]:
+				info['low'] = {'min': class0[0], 'max': class0[-1], 'number': len(class0)}
+			elif find_cat.index(class0[0]) in [2,3]:
+				info['med'] = {'min': class0[0], 'max': class0[-1], 'number': len(class0)}
+			else:
+				info['high'] = {'min': class0[0], 'max': class0[-1], 'number': len(class0)}
+
+			if find_cat.index(class1[0]) in [0,1]:
+				info['low'] = {'min': class1[0], 'max': class1[-1], 'number': len(class1)}
+			elif find_cat.index(class1[0]) in [2,3]:
+				info['med'] = {'min': class1[0], 'max': class1[-1], 'number': len(class1)}
+			else:
+				info['high'] = {'min': class1[0], 'max': class1[-1], 'number': len(class1)}
+
+			if find_cat.index(class2[0]) in [0,1]:
+				info['low'] = {'min': class2[0], 'max': class2[-1], 'number': len(class2)}
+			elif find_cat.index(class2[0]) in [2,3]:
+				info['med'] = {'min': class2[0], 'max': class2[-1], 'number': len(class2)}
+			else:
+				info['high'] = {'min': class2[0], 'max': class2[-1], 'number': len(class2)}
+			
+			all_info[category] = info
+			print(info)
+
+	info_file_pointer = open('/media/dell/Seagate Expansion Drive/CE_paper/Implementation/info.pkl', 'wb')
+	pickle.dump(all_info, info_file_pointer)
+
 def test():
 	data_folder = "/media/dell/Seagate Expansion Drive/CE_paper/Dataset/Amazon Dataset/categories/"
-	final_category_dir = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/categories/'
+	final_category_dir = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/price_scale/categories/'
 	category_names = io.list_dirs(data_folder)
 	get_price_scale(data_folder, final_category_dir, category_names)
 	
