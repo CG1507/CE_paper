ae181e3fe628f3a348e53476f7b2f491485c259f
Ghanshyam <g8ghanshym@gmail.com>
2018-09-02 18:50:43 +0530

    :recycle:

--- code/datasets/test.py
+++ code/datasets/test.py
@@ -1,5 +1,6 @@
 import datasets
 from utils import io
+from sentencelabel import core
 import gzip
 import json
 import pickle
@@ -34,11 +35,26 @@ def analyze_categories(data_folder, category_names):
 		print(line_no[category])
 		print(not_price[category])
 
+def see_data(data_folder, category_names):
+	for category in category_names:
+		file_pointer = parse(data_folder + category + '/meta.json.gz')
+		for line in file_pointer:
+			try:
+				json_line = json.loads(json.dumps(line))
+				keys = len(json_line.keys())
+				print(keys)
+			except:
+				pass
 
 def test():
 	data_folder = "/media/dell/Seagate Expansion Drive/CE_paper/Dataset/Amazon Dataset/categories/"
-	category_names = io.list_dirs(data_folder)
-	analyze_categories(data_folder, category_names)
+	#category_names = io.list_dirs(data_folder)
+	category_names = ['Electronics']
+	see_data(data_folder, category_names)
+	#analyze_categories(data_folder, category_names)
+	#result = core.get_sentiment('I love you!', on_base = "t", flag_prob=True)
+	#print(result)
+
 
 if __name__ == "__main__":
 	test()
