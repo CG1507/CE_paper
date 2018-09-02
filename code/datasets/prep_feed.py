1dd9a24c51da05a689aaeb6a9111f8a4a70ccfb3
Ghanshyam <g8ghanshym@gmail.com>
2018-09-03 05:14:51 +0530

    :rocket: initial commit

--- code/datasets/prep_feed.py
+++ code/datasets/prep_feed.py
@@ -5,10 +5,144 @@ import json
 import pickle
 import synch
 
+def get_attributes(json_line):
+	FLAG = True
+
+	if 'asin' in json_line
+		asin = json_line['asin']
+	else:
+		asin = ''
+
+	if 'helpful' in json_line:
+		helpful = json_line['helpful']
+	else:
+		helpful = [0, 0]
+
+	if 'reviewText'	in json_line:
+		reviewText = json_line['reviewText']
+	else:
+		reviewText = ''
+
+	if 'overall' in json_line:
+		overall = json_line['overall']
+	else:
+		overall = 0.0
+
+	if 'summary' in json_line:
+		summary = json_line['summary']
+	else:
+		summary = ''
+
+	if 'unixReviewTime' in json_line:
+		unixReviewTime = json_line['unixReviewTime']
+	else:
+		unixReviewTime = ''
+
+	if 'title' in json_line:
+		title = json_line['title']
+	else:
+		title = ''
+
+	if 'price' in json_line:
+		price = json_line['price']
+	else:
+		FLAG = False
+		price = ''
+
+	if 'related' in json_line:
+		related = json_line['related']
+		if "also_bought" not in related:
+			related["also_bought"] = []
+
+		if "also_viewed" not in related:
+			related["also_viewed"] = []
+
+		if "bought_together" not in related:
+			related["bought_together"] = []
+	else:
+		related = {"also_bought": [], "also_viewed": [], "bought_together": []}
+
+	if 'salesRank' in json_line:
+		salesRank = json_line['salesRank']
+	else:
+		FLAG = False
+		salesRank = ''
+
+	if 'brand' in json_line:
+		brand = json_line['brand']
+	else:
+		FLAG = False
+		brand = ''
+
+	if 'categories' in json_line:
+		categories = json_line['categories']
+	else:
+		FLAG = False
+		categories = [[]]
+
+	return FLAG, asin, helpful, reviewText, overall, summary, unixReviewTime, title, price, related, salesRank, brand, categories
+
+def get_pickle_object(file_path):
+	with open(file_path, 'rb') as f:
+		return pickle.load(f)
+
+def synch_data(paths, tmp_category_dir):
+	prev_category = ''
+	index = {}
+	index_file_pointer = None
+
+	path_no = 1
+	for path in paths:
+		category = path[1]
+		file_path = path[0]
+		meta_dir_path = tmp_category_dir + category + '/meta_chunks/'
+		
+		same_category_flag = True
+		if prev_category != category:
+			index_file_pointer = open(meta_dir_path + 'index.pkl', 'rb')
+			index = pickle.load(index_file_pointer)
+			same_category_flag = False
+		prev_category = category
+
+		prev_meta_file_no = ''
+		meta = {}
+
+		reading_file_pointer = io.read_file(file_path)
+		line_no = 1
+		for line in reading_file_pointer:
+			try:
+				json_line = json.loads(line)
+				asin = json_line['asin']
+				meta_file_no = str(index[asin])
+
+				if prev_meta_file_no != meta_file_no:
+					meta = get_pickle_object(meta_dir_path + 'meta' + meta_file_no + '.pkl')
+				else:
+					if not same_category_flag:
+						meta = get_pickle_object(meta_dir_path + 'meta' + meta_file_no + '.pkl')
+				prev_meta_file_no = meta_file_no
+				
+				product_json = meta[asin]
+				merged_json = dict(json_line, **product_json)
+				FLAG, asin, helpful, reviewText, overall, summary, unixReviewTime, title, price, related, salesRank, brand, categories = get_attributes(merged_json)
+				
+				line_no += 1
+				same_category_flag = True
+			except:
+				print('🐛 ERROR:\nFILE_PATH:', file_path, '\nLINE_NO:', line_no, '\nLINE:', line)
+				pass
+		print('Path no.', path_no, 'DONE:', file_path)
+		path_no += 1
+
+def write_feed_data(tmp_category_dir, category_names):
+	paths = synch.get_sorted_files_path(tmp_category_dir, category_names)
+	print('Total number of files:', len(paths))
+	synch_data(paths, tmp_category_dir)
+
 def test():
 	tmp_category_dir = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/tmp_data/categories/'
 	category_names = ['Electronics']
-	paths = get_sorted_files_path(tmp_category_dir, category_names)
+	write_feed_data(tmp_category_dir, category_names)
 
 if __name__ == "__main__":
 	test()
