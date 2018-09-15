7124aba8275714c256a2dcc0bcb38f0e8ac2ba6e
Ghanshyam <g8ghanshym@gmail.com>
2018-09-16 03:10:49 +0530

    :rocket: training updated

--- code/model/training.py
+++ code/model/training.py
@@ -1,4 +1,23 @@
 import model
+from utils import io
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+def normalize_data(data_file_path):
+	dataset = pd.read_csv(data_file_path)
+	values = dataset.values	
+	print('Done')
+
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	scaled = scaler.partial_fit(values)
+
+	print(scaled[0])
+	#writing_data_file_pointer  = io.create_file('/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0_norm.csv')
+
+
+def test():
+	data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0.csv'
+	normalize_data(data_file_path)
 
 if __name__ == "__main__":
 	test()
