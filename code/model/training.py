3914f7ecf859672d5470d3ddcbc0d405b68ac1d0
Ghanshyam <g8ghanshym@gmail.com>
2018-09-20 00:38:19 +0530

    :rocket: training changed

--- code/model/training.py
+++ code/model/training.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import math
 import conv_lstm
-from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau, EarlyStopping, LearningRateScheduler
 
 def save_normalize_data(data_file_path):
 	dataset = pd.read_csv(data_file_path)
@@ -40,7 +40,7 @@ class KerasBatchGenerator(object):
 			x = np.zeros((self.batch_size, self.time_step, self.rows, self.cols, self.channel))
 			y = np.zeros((self.batch_size, self.time_step, self.rows, self.cols, self.channel))
 			
-			if self.index + (self.rows * (self.time_step + 1) * self.batch_size) >= self.total_length:
+			if self.index + ((self.rows + self.time_step + 1) * self.batch_size) >= self.total_length:
 				self.index = 0
 
 			for i in range(self.batch_size):
@@ -59,7 +59,6 @@ class KerasBatchGenerator(object):
 					x[i, j, :, :, :] = rows_temp
 					y[i, j - 1, :, :, :] = rows_temp
 
-			for i in range(self.batch_size):
 				rows_temp = []
 				for k in range(self.rows):
 					cols_temp = []
@@ -69,20 +68,18 @@ class KerasBatchGenerator(object):
 							channel_temp.append(self.data[k + self.index][l])
 						cols_temp.append(channel_temp)
 					rows_temp.append(cols_temp)
-				self.index += 1
 				y[i, -1, :, :, :] = rows_temp
-			self.index -= (self.rows * self.batch_size)
-				
+
 			yield x, y
 
-def loss_callback():
+def lr_schedule(epoch):
 	initial_lrate = 0.1
 	drop = 0.5
 	epochs_drop = 10.0
 	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
 	return lrate
 
-def train_it(data_file_path):
+def train_it(train_data_file_path, validation_data_file_path):
 	batch_size = 1
 	time_step = 100
 	rows = 10
@@ -90,28 +87,35 @@ def train_it(data_file_path):
 	channel = 1
 	weight_path = None
 
-	train_data_generator = KerasBatchGenerator(data_file_path, batch_size, time_step, rows, cols, channel)
+	train_data_generator = KerasBatchGenerator(train_data_file_path, batch_size, time_step, rows, cols, channel)
+	validation_data_generator = KerasBatchGenerator(validation_data_file_path, batch_size, time_step, rows, cols, channel)
 	model = conv_lstm.conv_lstm_2d(weight_path)
-	
-	tensorboard_call = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=batch_size, write_graph=True, write_grads=True, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None)
-	modelcheckpoint_call = ModelCheckpoint('weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
-	earlystopping_call = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto', baseline=None)
-	lrdecay_call = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
 
-	model.fit_generator(train_data_generator.generate(), steps_per_epoch=10, epochs=1, 
-							verbose=1, callbacks=[tensorboard_call, modelcheckpoint_call, earlystopping_call, lrdecay_call], validation_data=None, validation_steps=None, class_weight=None, 
-							max_queue_size=10, workers=1, use_multiprocessing=False, 
-							shuffle=True, initial_epoch=0)
+	#change period	
+	modelCheckpoint_call = ModelCheckpoint('weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='auto', period=1)
+	earlyStopping_call = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
+	lrDecay_call = LearningRateScheduler(lr_schedule, verbose=1)
+	tensorboard_call = TensorBoard(log_dir='./logs', histogram_freq=1, batch_size=batch_size, write_graph=True, write_grads=True, write_images=True)
+	reduceLROnPlateau_call = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, mode='auto')
+
+	model.fit_generator(train_data_generator.generate(),
+						steps_per_epoch=train_data_generator.total_length//(batch_size*(time_step + rows)), epochs=1, 
+						callbacks=[modelCheckpoint_call, earlyStopping_call, lrDecay_call, tensorboard_call, reduceLROnPlateau_call],
+						validation_data=validation_data_generator.generate(),
+						validation_steps=validation_data_generator.total_length//(batch_size*(time_step + rows)),
+						class_weight=None, max_queue_size=10, workers=1, use_multiprocessing=False, 
+						verbose=1, shuffle=True, initial_epoch=0)
 	
-	model.save('final_lstm_reg.model')
+	model.save('final_save.model')
 	print('MODEL SAVED!')
 
 def test():
-	data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0.csv'
-	norm_data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0_norm.csv'
+	#data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0.csv'
+	#norm_data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/dataset_0_norm.csv'
 	#save_normalize_data(data_file_path)
-	small_data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/small_data.csv'
-	train_it(small_data_file_path)
+	small_train_data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/small_train_data.csv'
+	small_validation_data_file_path = '/media/dell/Seagate Expansion Drive/CE_paper/Implementation/final_data/small_validation_data.csv'
+	train_it(small_train_data_file_path, small_validation_data_file_path)
 
 if __name__ == "__main__":
 	test()
