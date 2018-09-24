b24f699beda4dddd6d8431f233aa0857dc785b1c
Ghanshyam <g8ghanshym@gmail.com>
2018-09-24 05:43:33 +0530

    :fire:

--- code/graphs/test_result.py
+++ code/graphs/test_result.py
@@ -1,4 +1,13 @@
 import graphs
+from utils import io
+import pandas as pd
+import numpy as np
+from keras.models import load_model
+
+def test():
+	model_file_path = ''
+	validation_data_file_path = 'gdrive/My Drive/colab_training/final_data/validation_dataset_0_norm.csv'
+
 
 if __name__ == "__main__":
 	test()
